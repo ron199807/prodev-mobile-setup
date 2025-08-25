39690e23f561760d2d34b197c077238841499b57
ron199807 <ronaldmweema@gmail.com>
2025-08-25 17:17:32 +0200

    Implementing Mobile Components in React Native

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -4,7 +4,6 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Entry Screen - Awesome</Text>
-      <View>
         <Text style={styles.largeText}>
           Typescript is great if you practice more
         </Text>
@@ -12,7 +11,6 @@ export default function Index() {
           React Native provides you a single codebase for cross platforms
         </Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
     </View>
   );
 }
